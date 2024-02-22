4fd65d60da9c56ae08426389a18508e2e7e9889a
Vava_62600 <120785753+Vava62600@users.noreply.github.com>
2024-02-22 15:05:15 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ClassBook'
+copyright = '2024, The ClassBook Devloppers'
+author = 'The ClassBook Devloppers'
 
 release = '0.1'
 version = '0.1.0'
